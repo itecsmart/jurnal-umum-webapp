8fb255ddfc399798585097879ddf4315e13a5bbf
grithaq <afifnurazizi@gmail.com>
2022-11-17 18:18:03 +0800

    CU-3arnk0p - As User, I will be able to create new jurnal

--- src/blueprint/jurnal_blueprint.py
+++ src/blueprint/jurnal_blueprint.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 from flask_login import login_required
 from models.jurnal import Jurnal
+from form.form_jurnal import JurnalForm
+from extensions import *
 
 jurnal_bp = Blueprint('jurnal_bp', __name__)
 
@@ -9,3 +11,15 @@ jurnal_bp = Blueprint('jurnal_bp', __name__)
 def jurnal():
     jurnals = Jurnal.query.all()
     return render_template('jurnal.html', jurnals=jurnals)
+
+
+@jurnal_bp.route('/jurnal/create', methods=['GET', 'POST'])
+def create_jurnal():
+    if request.method == 'POST':
+        form = JurnalForm()
+        jurnal = Jurnal(name=form.name.data, description=form.description.data)
+        db.session.add(jurnal)
+        db.session.commit()
+        return redirect('/jurnal')
+    form = JurnalForm()
+    return render_template('jurnal_form.html', form=form)
--- src/form/form_jurnal.py
+++ src/form/form_jurnal.py
@@ -3,5 +3,5 @@ from wtforms import StringField
 from wtforms.widgets import TextInput
 
 class JurnalForm(FlaskForm):
-    name = StringField("Jurnal Name", widget=TextInput)
-    description = StringField("Jurnal Name", widget=TextInput)
+    name = StringField("Jurnal Name", widget=TextInput())
+    description = StringField("Jurnal Description", widget=TextInput())
