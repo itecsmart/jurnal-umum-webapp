b9b5cfaed477727f86abc15aff8cf8f48952a9e6
grithaq <afifnurazizi@gmail.com>
2022-11-18 17:32:10 +0800

    CU-3arnkge - As User, I will be able to view detail transaction of jurnal

--- src/blueprint/transaction_blueprint.py
+++ src/blueprint/transaction_blueprint.py
@@ -5,5 +5,18 @@ transaction_bp = Blueprint('transaction_bp',__name__)
 
 @transaction_bp.route('/jurnal/<int:id>/transaction')
 def transaction(id):
-    transactions = Transaction.query.filter_by()
-    return render_template('transaction.html', id=id)
+    transactions = Transaction.query.filter(Transaction.jurnal_id==id).all()
+    jurnal = Jurnal.query.filter_by(id=id).first()
+    list_transaction = []
+    kas = 0
+    for t in transactions:
+        kas = kas + (t.debt-t.kredit)
+        data = {
+            'date': t.date,
+            'description': t.description,
+            'debt': t.debt,
+            'kredit': t.kredit,
+            'kas':kas
+        }
+        list_transaction.append(data)
+    return render_template('transaction.html', id=id, list_transaction=list_transaction, jurnal=jurnal, kas=kas)
--- src/models/transaction.py
+++ src/models/transaction.py
@@ -6,8 +6,8 @@ class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date = db.Column(Date)
     description = db.Column(db.String(100))
-    debt = db.Column(db.Integer)
-    kredit = db.Column(db.Integer)
+    debt = db.Column(db.Integer, default=0)
+    kredit = db.Column(db.Integer, default=0)
     jurnal_id = db.Column(db.Integer, db.ForeignKey('jurnal.id'))
 
     def __repr__(self):
