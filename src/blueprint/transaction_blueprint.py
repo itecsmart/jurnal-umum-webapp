c7791c70199a83d3b194263710a4599e31d0ee0c
grithaq <afifnurazizi@gmail.com>
2022-11-18 18:12:08 +0800

    CU-3arnmzx - As User, I will be able to delete transaction

--- src/blueprint/transaction_blueprint.py
+++ src/blueprint/transaction_blueprint.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, redirect, render_template, url_for
 from models import *
 
 transaction_bp = Blueprint('transaction_bp',__name__)
@@ -12,6 +12,7 @@ def transaction(id):
     for t in transactions:
         kas = kas + (t.debt-t.kredit)
         data = {
+            'id': t.id,
             'date': t.date,
             'description': t.description,
             'debt': t.debt,
@@ -20,3 +21,12 @@ def transaction(id):
         }
         list_transaction.append(data)
     return render_template('transaction.html', id=id, list_transaction=list_transaction, jurnal=jurnal, kas=kas)
+
+
+
+@transaction_bp.route('/jurnal/<int:id>/transaction/<int:t_id>/delete')
+def transaction_delete(id, t_id):
+    transaction=Transaction.query.filter_by(id=t_id).first()
+    db.session.delete(transaction)
+    db.session.commit()
+    return redirect(url_for('transaction_bp.transaction', id=id))
